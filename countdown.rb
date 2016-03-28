5530fe1c2cc36f6315098e7d4ad129cf9e0940b5
WigWog <andyzhao.zhao@gmail.com>
2016-03-28 17:16:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown(countdown)
+  until countdown == 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  if countdown == 0
+    "HAPPY NEW YEAR!"
+  end
+end
 
-def countdown
+def countdown_with_sleep(countdown)
+  until countdown == 0
+    puts "#{countdown} SECOND(S)!"
+    sleep(1)
+    countdown -= 1
+  end
+  if countdown == 0
+    "HAPPY NEW YEAR!"
+  end
 end
